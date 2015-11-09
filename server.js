4b656f675e0a905ff1db36ec83ff2b6b747c0661
Gregory Williams <gregoryew@gmail.com>
2015-11-08 20:39:29 -0500

    Update added

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/todos', function (req, res) {
 });
 
 app.get('/todos/:id', function (req, res) {
-	var todoid = parseInt(req.params.id);
+	var todoid = parseInt(req.params.id, 10);
 
 	var matchedTodo = _.findWhere(todos, {id: todoid});
 
@@ -55,6 +55,32 @@ app.delete('/todos/:id', function (req, res) {
 	} else {
 		res.status(404).json({"error": "no todo found with that id"});
 	}
+});
+
+app.put('/todos/:id', function (req, res) {
+	var todoid = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoid});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
 });
 
 app.listen(PORT, function() {
